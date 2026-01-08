c75b94e4c3e5e88fc987a960b03826df773a8ef6
frostzt <aidenfrostbite@gmail.com>
2026-01-09 01:34:53 +0530

    feat(pool-allocator): basic implementation of a pool allocator

--- lib/pool/pool.c
+++ lib/pool/pool.c
@@ -5,3 +5,53 @@
  * Date: 2026-01-09
  */
 
+#include "pool.h"
+
+Pool *pool_new(size_t pool_sz) {
+  Pool *pool = (Pool *)malloc(sizeof(Pool));
+  if (pool == NULL) {
+    return NULL;
+  }
+
+  pool->chunk_arr = pool->free_chunk = malloc(sizeof(Chunk) * pool_sz);
+  if (pool->chunk_arr == NULL) {
+    free(pool);
+    return NULL;
+  }
+
+  for (size_t i = 0; i < pool_sz - 1; i++) {
+    pool->chunk_arr[i].next = &pool->chunk_arr[i + 1];
+  }
+
+  pool->chunk_arr[pool_sz].next = NULL;
+  return pool;
+}
+
+void *pool_alloc(Pool *pool) {
+  if (pool == NULL || pool->free_chunk == NULL) {
+    return NULL;
+  }
+
+  Chunk *ptr = pool->free_chunk;
+  pool->free_chunk = pool->free_chunk->next;
+  return ptr;
+}
+
+void pool_free(Pool *pool, void *ptr) {
+  if (pool == NULL || ptr == NULL) {
+    return;
+  }
+
+  Chunk *new_free_chunk = (Chunk *)ptr;
+  new_free_chunk->next = pool->free_chunk;
+  pool->free_chunk = new_free_chunk;
+}
+
+void pool_destroy(Pool *pool) {
+  if (pool == NULL) {
+    return;
+  }
+
+  free(pool->chunk_arr);
+  free(pool);
+}
--- lib/pool/pool.h
+++ lib/pool/pool.h
@@ -5,3 +5,31 @@
  * Date: 2026-01-09
  */
 
+#include <stddef.h>
+#include <stdlib.h>
+
+#define CHUNK_SZ 64
+
+typedef union Chunk Chunk;
+union Chunk {
+  Chunk *next;
+  char arr[CHUNK_SZ];
+};
+
+typedef struct Pool Pool;
+struct Pool {
+  Chunk *free_chunk;
+  Chunk *chunk_arr;
+};
+
+/* creates a new pool */
+Pool *pool_new(size_t pool_sz);
+
+/* allocates a chunk from the pool and returns it back */
+void* pool_alloc(Pool* pool);
+
+/* frees the pointer and returns it back to the pool */
+void pool_free(Pool *pool, void *ptr);
+
+/* frees everything and destroys the pool */
+void pool_destroy(Pool *pool);
