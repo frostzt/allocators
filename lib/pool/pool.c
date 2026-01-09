9038f7f1bb00856bdc833a60402a28d9121c4d8a
frostzt <aidenfrostbite@gmail.com>
2026-01-09 16:49:06 +0530

    feat(pool): added ability to expand pool

--- lib/pool/pool.c
+++ lib/pool/pool.c
@@ -5,6 +5,8 @@
  * Date: 2026-01-09
  */
 
+#include <stdbool.h>
+
 #include "pool.h"
 
 Pool *pool_new(size_t pool_sz) {
@@ -13,17 +15,29 @@ Pool *pool_new(size_t pool_sz) {
     return NULL;
   }
 
-  pool->chunk_arr = pool->free_chunk = malloc(sizeof(Chunk) * pool_sz);
-  if (pool->chunk_arr == NULL) {
+  /* allocates the first free chunk array */
+  Chunk *arr = pool->free_chunk = malloc(sizeof(Chunk) * pool_sz);
+  if (arr == NULL) {
     free(pool);
     return NULL;
   }
 
+  /* initialize the arr and set the next ptrs */
   for (size_t i = 0; i < pool_sz - 1; i++) {
-    pool->chunk_arr[i].next = &pool->chunk_arr[i + 1];
+    arr[i].next = &arr[i + 1];
+  }
+  arr[pool_sz - 1].next = NULL;
+
+  /* allocate a new array start ptr and track it */
+  pool->array_starts = malloc(sizeof(ArrayStart));
+  if (pool->array_starts == NULL) {
+    free(arr);
+    free(pool);
+    return NULL;
   }
 
-  pool->chunk_arr[pool_sz].next = NULL;
+  pool->array_starts->next = NULL;
+  pool->array_starts->arr = arr;
   return pool;
 }
 
@@ -52,6 +66,47 @@ void pool_destroy(Pool *pool) {
     return;
   }
 
-  free(pool->chunk_arr);
-  free(pool);
+  ArrayStart *arr_start = pool->array_starts;
+  while (arr_start != NULL) {
+    ArrayStart *next = arr_start->next;
+    free(arr_start->arr);
+    free(arr_start);
+    arr_start = next;
+  }
+}
+
+bool pool_expand(Pool *pool, size_t extra_sz) {
+  if (pool == NULL) {
+    return NULL;
+  }
+
+  /* allocate this new requested block size */
+  Chunk *extra_arr = (Chunk *)malloc(sizeof(Chunk) * extra_sz);
+  if (extra_arr == NULL) {
+    return NULL;
+  }
+
+  /* link the blocks */
+  for (size_t i = 0; i < extra_sz - 1; i++) {
+    extra_arr[i].next = &extra_arr[i + 1];
+  }
+  extra_arr[extra_sz - 1].next = pool->free_chunk;
+  pool->free_chunk = extra_arr;
+
+  //                               f
+  // [x]->[x]->[x]->[x] -> NULL | []->[]->[]->[] --| (NULL)
+  //                       ^                       |
+  //                       |-----------------------|
+
+  /* allocate a new array block */
+  ArrayStart *new_arr_start = (ArrayStart *)malloc(sizeof(ArrayStart));
+  if (new_arr_start == NULL) {
+    free(extra_arr);
+    return NULL;
+  }
+
+  new_arr_start->arr = extra_arr;
+  new_arr_start->next = pool->array_starts;
+  pool->array_starts = new_arr_start;
+  return true;
 }
--- lib/pool/pool.h
+++ lib/pool/pool.h
@@ -16,20 +16,29 @@ union Chunk {
   char arr[CHUNK_SZ];
 };
 
+typedef struct ArrayStart ArrayStart;
+struct ArrayStart {
+  Chunk *arr;
+  ArrayStart *next;
+};
+
 typedef struct Pool Pool;
 struct Pool {
   Chunk *free_chunk;
-  Chunk *chunk_arr;
+  ArrayStart *array_starts;
 };
 
 /* creates a new pool */
 Pool *pool_new(size_t pool_sz);
 
 /* allocates a chunk from the pool and returns it back */
-void* pool_alloc(Pool* pool);
+void *pool_alloc(Pool *pool);
 
 /* frees the pointer and returns it back to the pool */
 void pool_free(Pool *pool, void *ptr);
 
 /* frees everything and destroys the pool */
 void pool_destroy(Pool *pool);
+
+/* expands the pool */
+bool pool_expand(Pool *pool, size_t extra_sz);
